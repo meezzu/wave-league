cc98ca495a765add65da15c20549f20f43c49738
Emmanuel Tukpe <tukpeemmanuel@gmail.com>
2022-01-17 16:32:07 +0100

    feat: added artiste data module

---
+++ src/data/artiste/artiste.model.ts
@@ -0,0 +1,6 @@
+export interface IArtiste {
+  artiste_name: string;
+  record_label: string;
+  avatar: string;
+  price: number;
+}
---
+++ src/data/artiste/index.ts
@@ -0,0 +1,4 @@
+export * from './artiste.model';
+export { default as ArtisteSchema } from './artiste.schema';
+export * from './artiste.repo';
+
