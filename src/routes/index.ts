bf8a03f12b2cef81cd8c8d232f202a46062b3c68
Emmanuel Tukpe <tukpeemmanuel@gmail.com>
2022-01-19 20:08:10 +0100

    feat: added getOnePlayer method in controller

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,5 +6,5 @@ const v1Router = Router();
 v1Router.get('/players', players.getPlayers);
 // v1Router.post('/players/login', gateman.guard(), users.login);
 v1Router.post('/players/signup', players.createPlayer);
-
+v1Router.get('/players/:id', players.getOnePlayer)
 export default v1Router;
