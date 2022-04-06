3e4d3efc709e61a5a9bae4d61f1e24655a635308
bossman-codex <56489079+bossman-codex@users.noreply.github.com>
2022-04-06 14:33:53 +0100

    filter by week

--- src/server/controllers/charts.controller.ts
+++ src/server/controllers/charts.controller.ts
@@ -7,13 +7,18 @@ export class ChartsController extends BaseController {
     try {
       const thisWeek = await WeekRepo.getModel().find().sort({ created_at: -1 }).limit(1)
 
+      const weeks = req.query.week
+        ? { week_number: req.query.week }
+        : {week_number: thisWeek[0].week_number};
+      
+      console.log(req.query.week)
       const query = {
-          week_number: thisWeek[0].week_number,
+         ...weeks
       };
 
       const artistes = await PointRepo.getPaged({
           query,
-          sort: req.query.sort|| 'created_at',
+          sort: req.query.sort|| '-points',
           page: Number(req.query.page),
           per_page: Number(req.query.per_page),
           populations: {path: "artiste", select:"artiste_name avatar record_label"}
