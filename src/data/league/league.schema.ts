88c3d34d7999dd198905189e6279571f3d142331
bossman-codex <56489079+bossman-codex@users.noreply.github.com>
2022-04-08 14:52:08 +0100

    formatted code

--- src/data/league/league.schema.ts
+++ src/data/league/league.schema.ts
@@ -1,26 +1,22 @@
-import {
-  requiredNumber,
-  SchemaFactory,
-  trimmedRequiredString,
-} from '../base';
+import { requiredNumber, SchemaFactory, trimmedRequiredString } from '../base';
 import { ILeague } from './league.model';
 
 const LeagueSchema = SchemaFactory<ILeague>({
   league_name: { ...trimmedRequiredString },
   league_type: {
     ...trimmedRequiredString,
-    enum: ['public', 'private'],
+    enum: ['public', 'private']
   },
-  player_limit: { ...requiredNumber, default: 20 , max:100 },
+  player_limit: { ...requiredNumber, default: 20, max: 100 },
   players: [
     {
-     player: {
-    ...trimmedRequiredString,
-    ref: 'Player',
-    unique: true
-  }
+      player: {
+        ...trimmedRequiredString,
+        ref: 'Player',
+        unique: true
+      }
     }
-    ]
+  ]
 });
 
 export default LeagueSchema;
