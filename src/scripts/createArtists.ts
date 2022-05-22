aca399021479b5122078ad156805bb25dcac5f2c
Raymond Tukpe <rtukpe@gmail.com>
2022-05-22 14:52:41 +0100

    feat: update create artistes script

--- src/scripts/createArtists.ts
+++ src/scripts/createArtists.ts
@@ -2,6 +2,7 @@ import { ArtisteRepo, IArtiste } from '../data/artiste';
 import faker from 'faker';
 import db from '../server/db';
 import Omi, { OmiEvent } from '@random-guys/omi';
+import { seedArtistes } from './artistes';
 
 db.connect();
 
@@ -26,21 +27,20 @@ omi.on(OmiEvent.ERROR, error => {
   console.log(error);
 });
 
-for (let index = 0; index < 100; index++) {
-  omi.addMany(
-    [...Array.from({ length: 10 })].map(() => {
-      return {
-        price: faker.random.arrayElement([5, 10, 15, 20]),
-        avatar: faker.image.people(),
-        record_label: faker.random.arrayElement([
-          'YBNL',
-          'Mavin',
-          'YMCMB',
-          'Plug',
-          'Chocolate City'
-        ]),
-        artiste_name: faker.name.findName()
-      };
-    })
-  );
-}
+omi.addMany(
+  seedArtistes.map(artiste_name => {
+    const name = artiste_name.replace(' ', '_');
+    return {
+      price: faker.random.arrayElement([5, 10, 15, 20]),
+      avatar: `https://res.cloudinary.com/raymond-tukpe/image/upload/v1653068719/wave-league/${name}.png`,
+      record_label: faker.random.arrayElement([
+        'YBNL',
+        'Mavin',
+        'YMCMB',
+        'Plug',
+        'Chocolate City'
+      ]),
+      artiste_name
+    };
+  })
+);
